af8dfad479842e8df8f30dc23bd5fea9b9347667
yel1337 <daniel.salvaloza2002@gmail.com>
2025-03-18 23:09:40 +0800

    class Messages: removed confusion on wether what index of messages to use

--- mellisa/mellisa/custom/log/log_custom.py
+++ mellisa/mellisa/custom/log/log_custom.py
@@ -1,17 +1,12 @@
+from misc.messages import Messages
 def log_level(self, message, datas=None, return_none_cb=None, return_data_cb=None):
     log_if_found_info = "[INFO]"
     log_if_warning = "[WARNING]"
+
+    msg = Messages()
     
-    if return_data_cb:
+    if message in msg.for_info:
+        return log_if_found_info
+    elif message in msg.for_warning:
         return log_if_warning
-    elif return_none_cb == 0:
-        return log_if_warning 
-    
-    if message == "Scraping...":
-        return log_if_found_info
-    elif message == "Saving...":
-        return log_if_found_info
-    elif message == "PARAMETERS FOUND":
-        return log_if_found_info
-    elif message == "Page might not contain any parameter/s to be extracted, maybe try another one?":
-        return log_if_warning
+   
