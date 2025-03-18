1ab1dc51a9d94fe1c644bce212ec605bfdc721de
yel1337 <daniel.salvaloza2002@gmail.com>
2025-03-18 23:11:51 +0800

    Additional file for messages

---
+++ mellisa/mellisa/misc/messages.py
@@ -0,0 +1,7 @@
+class Messages:
+    for_info =  ["Crawling...", 
+                 "Scraping...", 
+                 "Saving...", 
+                 "PARAMETERS FOUND", 
+                 "Total number of scraped parameter/s from page"]
+    for_warning = ["Page might not contain any parameter/s to be extracted, maybe try another one?"]
