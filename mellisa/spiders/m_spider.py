b4cdaf54a0b7145f17dc13601090c2e1e8ad2cde
yel1337 <daniel.salvaloza2002@gmail.com>
2025-04-27 09:46:54 +0800

    added function for spider to look for hyperlinks to crawl for next pages within the single page
    - crawl_page()
    - add_val_url()

--- mellisa/spiders/m_spider.py
+++ mellisa/spiders/m_spider.py
@@ -27,41 +27,57 @@ class ScrapeParameters(scrapy.Spider):
             ]
 
     # Return Num Callback
-
     def return_num_data(self, data_len):
         if data_len > 0:
             return True
 
     # Return None Callback
-
     def return_none(self, data_len):
         if data_len == 0:
             return True
 
-    def _add_val_(self, datas, response):
+    def _add_val_item(self, datas, response):
         loader = ItemLoader(item=MellisaItem(), response=response)
         loader.add_value("item_param", datas)
 
         return loader.load_item()
 
+    def _add_val_url(self, datas, response):
+        loader = ItemLoader(item=MellisaItem(), response=response)
+        loader.add_value("urls", datas)
+
+    def crawl_page(self, query, response):
+        next_pages = response.xpath(query)
+        urls = next_pages.getall()
+
+        for url in urls:
+            if url:
+                yield response.follow(url, callback=self.parse)
+
     def parse(self, response):
         # assumes wordlist.txt is in the same dir as m_spiders.py's parent (i.e. /mellicd sa/)
         path = Path(__file__).resolve().parent.parent / "wordlist.txt"
+        query = Path(__file__).resolve().parent.parent / "page_queries.txt"
+
         xpaths = self.load_xpath(path)
+        query_xpath = self.load_xpath(query)
 
         if isinstance(xpaths, list):
             xpaths = xpaths[0] if xpaths else ""
+        
+        if isinstance(query_xpath, list):
+            query_xpath = query_xpath[0] if query_xpath else ""
 
-        extracted_datas = response.xpath(xpaths).extract()
+        extracted_datas = response.xpath(xpaths).getall()
         self.datas.extend(extracted_datas)
 
-        load_item = self._add_val_(extracted_datas, response)
+        load_item = self._add_val_item(extracted_datas, response)
         yield load_item
 
-        if load_item:
-            next_page = response.xpath("//li/a/@href").get()
-            if next_page:
-                yield response.follow(next_page, callback=self.parse)
+        crawl_page = self.crawl_page(query_xpath, response)
+        load_url = self._add_val_url(crawl_page, response)
+
+        yield load_url
 
     def closed(self, reason):
         misc = Misc()
