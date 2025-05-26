64e3c3480300463f0b84c6930ecfb60a0ccc8f4b
yel <yel@localhost.localdomain>
2025-05-26 13:31:15 +0800

    proper event handler from main to misc/

--- mellisa_base/misc/misc_prompts.py
+++ mellisa_base/misc/misc_prompts.py
@@ -1,54 +1,70 @@
 import time
+from mellisa import main, run_spider
 from datetime import datetime
-from custom.log.log_custom import log_level
+from typing import Optional
+from spiders.m_spider import ScrapeParameters
+from custom.log.log_custom import log_level, log
 from misc.messages import Messages
 from pathlib import Path
 
-msg = Messages()
-
 
 class Misc:
+    def __init__(self, len_of_data, data, value_main):
+        self.output_log = log
+        self.msg = Messages()
+        self.data_len = len_of_data
+        self.data = data
+        self.value_main = value_main
+
     def time_of_execution(self, message, datas=None, return_none_callback=None, return_data_callback=None):
         execution_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        if return_data_callback is None:
-            def return_data_callback(): return bool(datas)
-        if return_none_callback is None:
-            def return_none_callback(): return not bool(datas)
-
-        log = log_level(datas, message, return_none_callback,
+        logger = log_level(datas, message, return_none_callback,
                         return_data_callback)
 
-        if message in msg.for_info:
-            return f"{execution_time} {log} {message}"
-        elif message in msg.for_warning:
-            return f"{execution_time} {log} {message}"
+        formatted_message = f"{execution_time} {logger} {message}"
+        return formatted_message
 
     def misc_start(self):
-        print(self.time_of_execution(msg.for_info[0]))
+        start_result = self.time_of_execution(f"{self.msg.for_info[0]}")
         time.sleep(2)
+        return start_result
 
     def misc_none(self):
-        print(self.time_of_execution(msg.for_warning[0]))
+        none_result = self.time_of_execution(f"{self.msg.for_warning[0]}")
+        return none_result
 
     def misc_has_len(self, data_len):
-        return f"{msg.for_info[4]} {data_len}"
+        return f"{self.msg.for_info[3]} {data_len}"
+
+    def misc_on_default(self):
+        return f"{self.msg.for_info[4]}"
 
     def misc_output(self):
         print("")
         output_filePath = Path(__file__).resolve().parent.parent / "output"  
         print(f"Output file saved in: {output_filePath}/domain_name.json")
 
-    def misc_saving(self, datas, data_len, return_none_callback=None, return_data_callback=None):
-        if datas:
+    def misc_saving(self, data_len, main_val, return_none_callback=None, return_data_callback=None):
+        crawling = self.time_of_execution(self.msg.for_info[0], self.data, return_data_callback)
+        print(f"{crawling}")
+
+        if self.value_main is None:
+            default = self.time_of_execution(self.msg.for_info[4])
+            print(f"{default}")
+        elif self.value_main is True:
+            custom = self.time_of_execution(self.msg.for_info[5])
+            print(f"{custom}")
+
+        if self.data:
             print(self.time_of_execution(
-                msg.for_info[2], datas, return_data_callback))
+                self.msg.for_info[1], self.data, return_none_callback, return_data_callback))
+        if run_spider:
             print(self.time_of_execution(
-                msg.for_info[1], datas, return_none_callback, return_data_callback))
-            print(self.time_of_execution(
-                msg.for_info[2], datas, return_none_callback, return_data_callback))
-            time.sleep(2)
-            print(f"{self.time_of_execution(msg.for_info[4])}: {data_len}")
+                self.msg.for_info[2], self.data, return_none_callback, return_data_callback))
+        time.sleep(2)
+        if self.data_len:
+            print(f"{self.time_of_execution(self.msg.for_info[6])}: {self.data_len}")
             time.sleep(1)
-        elif not datas:
+        elif not self.data:
             raise Exception("No scraped data from pipeline")
